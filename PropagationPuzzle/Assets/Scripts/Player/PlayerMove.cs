a3304984bf1951fd076726fbfff76a92949e77f3
oskaroskaroskaroskaroskar <ZQ21JJ@student.aau.dk>
2026-03-24 14:14:46 +0100

    fixedupdate

--- PropagationPuzzle/Assets/Scripts/Player/PlayerMove.cs
+++ PropagationPuzzle/Assets/Scripts/Player/PlayerMove.cs
@@ -9,20 +9,29 @@ public class PlayerMove : MonoBehaviour
         
     }
 
-    void LateUpdate()
+    void FixedUpdate()
     {
         Vector2 movementInput = Rotate(PlayerInput.instance.PlayerMovement.normalized, -transform.rotation.eulerAngles.y);
 
-        Move(movementInput * playerStats.MoveSpeed * Time.deltaTime);
+        Move(movementInput * playerStats.MoveSpeed * Time.fixedDeltaTime);
     }
     void Move(Vector2 movement)
     {
-        Vector2 movingSpeed = new Vector2(rigid.linearVelocity.x + movement.x, rigid.linearVelocity.z + movement.y) ;
-        if (movingSpeed.magnitude <= playerStats.MaxSpeed)
+           
+       
+            
+        Vector3 accVector = new Vector3(movement.x, 0, movement.y);
+        Debug.Log(accVector);
+        rigid.AddForce(accVector, ForceMode.Acceleration);
+
+        Vector2 movingSpeed = new Vector2(rigid.linearVelocity.x, rigid.linearVelocity.z);
+        if (movingSpeed.magnitude > playerStats.MaxSpeed)
         {
-            Vector3 accVector = new Vector3(movement.x, 0, movement.y);
-            rigid.AddForce(accVector, ForceMode.Acceleration);
+            Vector2 movingDirection = movingSpeed.normalized;
+            Vector2 movingSpeedCapped = movingDirection * playerStats.MaxSpeed;
+            rigid.linearVelocity = new Vector3(movingSpeedCapped.x, rigid.linearVelocity.y, movingSpeedCapped.y);
         }
+
     }
     public static Vector2 Rotate(Vector2 vector, float degrees)
     {
