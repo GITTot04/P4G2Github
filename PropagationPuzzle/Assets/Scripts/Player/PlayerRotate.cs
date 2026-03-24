149c9d4bcc856489f60196f3a8e64592947f0fe4
oskaroskaroskaroskaroskar <ZQ21JJ@student.aau.dk>
2026-03-24 13:10:14 +0100

    playercontroller

---
+++ PropagationPuzzle/Assets/Scripts/Player/PlayerRotate.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerRotate : MonoBehaviour
+{
+    public GameObject playerBody;
+    public PlayerStats playerStats;
+    Vector2 lastMousePosition;
+
+    void LateUpdate()
+    {
+        Vector2 rotate = PlayerInput.instance.MouseMovement;
+
+        RotateHorizontal(rotate.x*playerStats.LookSpeed);
+        RotateVertical(-rotate.y*playerStats.LookSpeed);
+    }
+
+    void RotateHorizontal (float amount)
+    {
+        playerBody.transform.Rotate(new Vector3(0f, amount, 0f));
+    }
+
+    void RotateVertical (float amount)
+    {
+        if (transform.rotation.eulerAngles.x + amount < 90 || transform.rotation.eulerAngles.x + amount > 270)
+        {
+            transform.Rotate(new Vector3(amount, 0f, 0f));
+        }
+        
+    }
+}
