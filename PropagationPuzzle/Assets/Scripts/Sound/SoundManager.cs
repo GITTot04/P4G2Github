7c86a2c16735566dea236e942e5eeca3240da9c7
GITTot04 <112081156+GITTot04@users.noreply.github.com>
2026-05-05 12:27:36 +0200

    Amplifier plate indicator and locked cursor

--- PropagationPuzzle/Assets/Scripts/Sound/SoundManager.cs
+++ PropagationPuzzle/Assets/Scripts/Sound/SoundManager.cs
@@ -24,6 +24,7 @@ public class SoundManager : MonoBehaviour
             SceneManager.sceneLoaded += FindDoorSensor;
             SceneManager.sceneLoaded += SetAllowedAmplifiers;
             SceneManager.sceneLoaded += SetPlayerOnAction;
+            Cursor.lockState = CursorLockMode.Locked;
         }
     }
 
